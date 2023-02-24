0a894f89a4ac1154d77c44e01762a01af205ef91
Clécio Sousa <cleciosouza015@gmail.com>
2023-02-23 21:02:56 -0300

    adicionando o carousel do Gliter.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 const buttonIcon = document.querySelector('#menu-icon')
 const buttonIconClose = document.querySelector('#icon-close')
-
 const menu = document.querySelector('#menu')
+const carousel = document.querySelector('.js-carousel')
 
 buttonIcon.addEventListener('click', () =>{
     menu.classList.add('active')
@@ -9,4 +9,24 @@ buttonIcon.addEventListener('click', () =>{
 
 buttonIconClose.addEventListener('click', () =>{
     menu.classList.remove('active')
+})
+
+new Glider(carousel, {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    arrows: {
+        prev: '.js-carousel__prev',
+        next: '.js-carousel__next',
+    },
+    scrollLock: true,
+    responsive: [
+        {
+            breakPoint: 1025,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        }
+    ]
 })
